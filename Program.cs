462757a94e28f2192c1aeacff892310caa5ec117
aratushna <94103037+aratushna@users.noreply.github.com>
2023-03-23 22:38:59 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Hashtable;
+
+namespace HashTable;
+using System;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var dictionary = new Dictionary();
+        var lines = File.ReadAllLines("C:/Users/rsp/RiderProjects/ConsoleApp5/ConsoleApp5/dictionary.txt");
+        int i = 0;
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split(';');
+            string key = parts[0].Trim();
+            string value = parts[1].Trim();
+            dictionary.Add(key, value);
+            if (i % 1000 == 0) Console.WriteLine("Number of loaded words " + i);
+            i++;
+        }
+        
+        Console.WriteLine("Enter a word:");
+        string word = Console.ReadLine();
+        string meaning = dictionary.Get(word);
+        if (meaning != null)
+        {
+            Console.WriteLine($"'{word}' means: {meaning}");
+        }
+        else
+        {
+            Console.WriteLine($"'{word}' was not found.");
+        }
+    }
+}
